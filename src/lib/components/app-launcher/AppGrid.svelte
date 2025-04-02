559784409a8f583b52751fbb15785aa18173dbea
ZuiverZuigen <2063919aerden@zuyd.nl>
2025-04-02 15:21:01 +0200

    App Launcher aangepat

--- src/lib/components/app-launcher/AppGrid.svelte
+++ src/lib/components/app-launcher/AppGrid.svelte
@@ -28,17 +28,19 @@
   </script>
   
   <!-- UI-template -->
-  <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-    {#each visibleApps as app}
-      <a
-        href={app.href}
-        class="flex items-center space-x-4 p-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-gray-100 dark:hover:bg-gray-700 transform hover:-translate-y-1 transition duration-150"
-      >
-        <!-- Toon het icoon en de naam van de app -->
-        <div class="text-3xl">{app.icon}</div>
-        <div class="text-lg font-medium text-gray-800 dark:text-gray-200">{app.name}</div>
-      </a>
-    {/each}
+  <div class="flex flex-col justify-end h-full">
+    <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {#each visibleApps as app}
+        <a
+          href={app.href}
+          class="flex items-center space-x-4 p-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-gray-100 dark:hover:bg-gray-700 transform hover:-translate-y-1 transition duration-150"
+        >
+          <!-- Toon het icoon en de naam van de app -->
+          <div class="text-3xl">{app.icon}</div>
+          <div class="text-lg font-medium text-gray-800 dark:text-gray-200">{app.name}</div>
+        </a>
+      {/each}
+    </div>
   </div>
   
   <style>
@@ -46,4 +48,3 @@
       transition: all 0.2s ease-in-out;
     }
   </style>
-  
